4940d421192fd0a712fd781808647dd9768b9f94
dit1304 <43095037+dit1304@users.noreply.github.com>
2025-03-02 04:04:01 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Bot Configuration
+BOT_TOKEN = '8195505675:AAEb5lUIh8Pf46_g8XQT_C3yUlKYGg3AT1E'  # Replace with your actual bot token
+BOT_USERNAME = 'zeroCC'  # Replace with your bot's username
+
+# API Keys and Tokens
+GOOGLE_API_KEY = "AIzaSyADbwRHdEK-J5sqgtvaVv4Kz_H9ymax7KE"
+SEARCH_ENGINE_ID = "96750e7f8dfab47e1"
+IPINFO_TOKEN = "4dd04ebcd1b3c1"
+
+
+
